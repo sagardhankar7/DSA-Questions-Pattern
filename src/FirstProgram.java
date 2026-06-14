b31ff1fa1022449818dd3fb9f321eb20acd5ccef
sagardhankar7 <sag9315@gmail.com>
2026-06-14 09:21:29 +0530

    Initial commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,5 @@
+public class FirstProgram {
+    static void main() {
+        System.out.println("Hi, This program");
+    }
+}
---
+++ src/LinkedList/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode();
+        ListNode l2 = new ListNode();
+        
+    }
+}
