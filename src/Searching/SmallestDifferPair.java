ff31dee99fe3eccce12514092a5b656971d1284b
sagardhankar7 <sag9315@gmail.com>
2026-07-05 09:51:34 +0530

    DSA Questions Searching Sorting Minimum difference between two arrays

--- src/Searching/SmallestDifferPair.java
+++ src/Searching/SmallestDifferPair.java
@@ -1,4 +1,35 @@
 package Searching;
 
+import java.util.Arrays;
+
 public class SmallestDifferPair {
+
+    public static int smallestDifferencePairOptimize(int[] ar1, int n, int[] ar2, int m) {
+        Arrays.sort(ar1);
+        Arrays.sort(ar2);
+
+        int a=0,b=0;
+        int result = Integer.MAX_VALUE;
+        while (a<n&&b<m) {
+            int dif = Math.abs(ar1[a]-ar2[b]);
+            if (dif<result) result=dif;
+
+            if (ar1[a]<ar2[b]) a++;
+            else b++;
+        }
+
+        return result;
+    }
+
+    public static int smallestDifferencePair(int[] ar1, int n, int[] ar2, int m) {
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                //ith from ar1 and jth from ar2
+                int dif = Math.abs(ar1[i] - ar2[j]);
+                if (dif < result) result = dif;
+            }
+        }
+        return result;
+    }
 }
