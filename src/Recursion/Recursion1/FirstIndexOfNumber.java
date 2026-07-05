3adb882ac1ad42a92671eadaf7eac4c39983a951
sagardhankar7 <sag9315@gmail.com>
2026-07-05 12:31:59 +0530

    DSA Questions Recursion First Index of a Number in array

---
+++ src/Recursion/Recursion1/FirstIndexOfNumber.java
@@ -0,0 +1,18 @@
+package Recursion.Recursion1;
+
+public class FirstIndexOfNumber {
+    public static int firstIndex(int input[], int x) {
+        return helper(input, 0, x);
+
+    }
+
+    static int helper(int[] input, int index, int x) {
+        if (index==input.length) return -1;
+        if (input[index]==x) return index;
+
+        return helper(input, index+1, x);
+
+
+
+    }
+}
